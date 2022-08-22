313b648b5706a20aa69b9d402d590c0bc668beda
CJINBNH <alex_egorov_89@mail.ru>
2022-08-22 17:36:03 +0400

    Completed task_035

--- task_035/Program.cs
+++ task_035/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123 ([5, 18, 123, 6, 2] -> 1; [1, 2, 3, 6, 2] -> 0)
+
+Console.Clear();
+int[] array = new int[10];
+Random random = new Random();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 123);
+    if (array[i] >=10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine($"Количество элементов массива в диапазоне от 10 до 99 равно: {count}");
