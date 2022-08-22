719ac3c91b2172961934c085b9207305e8fa3776
CJINBNH <alex_egorov_89@mail.ru>
2022-08-22 17:49:43 +0400

    Completed task_036

--- task_036/Program.cs
+++ task_036/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+int[] array = new int[10];
+Random random = new Random();
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 10);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+for (int j = 1; j < array.Length; j = j + 2)
+    {
+        sum = sum + array[j];
+    }
+Console.WriteLine($"Сумма элементов массива с нечеными индексами равна: {sum}");
